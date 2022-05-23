7440f266fc8f19fb1b9ad74d14ca41a3f48c73c0
Anikroy7 <roy645800@gmail.com>
2022-05-23 19:32:34 +0600

    tools api created

--- index.js
+++ index.js
@@ -13,6 +13,31 @@ app.get('/', (req, res) => {
     res.send('handy plus server is running!')
 })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7wku5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db('handyPlus').collection('tools');
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const result = await toolsCollection.find(query).toArray();
+            res.send(result)
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+// call run function
+
+run().catch(console.dir);
+
+
 app.listen(port, () => {
     console.log(`Handy plus on port ${port}`)
 })
