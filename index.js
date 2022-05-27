75ac2bf50ba205e5f48cf1f730f6a4d59af5eed6
Anikroy7 <roy645800@gmail.com>
2022-05-27 13:24:13 +0600

    tools delete api implement

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
             res.send(result)
 
         })
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) }
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result)
+        })
     }
     finally {
         // await client.close();
