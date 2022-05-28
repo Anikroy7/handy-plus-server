7af137117f06f91ab792432f7d30d699c141a52a
Anikroy7 <roy645800@gmail.com>
2022-05-28 16:09:14 +0600

    payment api implement for specific tool"

--- index.js
+++ index.js
@@ -150,6 +150,12 @@ async function run() {
             }
 
         })
+        app.get('/orders/payment/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.findOne(query);
+            res.send(result)
+        })
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
 
